cdb0f580b4d43355f05a97c463066c2411119ec7
Henrique Domingos <henrique@leoa.com.br>
2023-02-14 04:07:12 -0500

    Initial commit

---
+++ src/App.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+
+describe("It's need to be installed", ()=>{
+  test('renders learn react link', () => {
+    render(<App />)
+    const linkElement = screen.getByText(/learn react/i)
+    expect(linkElement).toBeInTheDocument()
+  });
+})
