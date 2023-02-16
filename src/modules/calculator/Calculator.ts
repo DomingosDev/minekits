b931cf614590105e9a6a227c5db3305bfd8d0799
Henrique Domingos <henrique@leoa.com.br>
2023-02-16 05:00:07 -0500

    feat: Enchantment strategy and cost calculator

---
+++ src/modules/calculator/Calculator.ts
@@ -0,0 +1,122 @@
+import { Enchantment } from "../kits/Enchantment";
+import { Item } from "../kits/Item";
+import { Kit } from "../kits/Kit";
+
+export default class Calculator{
+    calculateKit(kit: Kit = Kit.current){
+        return kit.items.reduce((result:number, item:Item) => {
+            return result + this.calculateItem(item);
+        }, 0);
+    }
+
+    calculateItem(item: Item){
+        return this.calculateTotal(this.getItemEnchantPath(item));
+    }
+
+    calculateTotal(path: Array<any>){
+        return path.reduce( (result:number, item: any) => {
+            result += item[2];
+            return result;
+        }, 0)
+    };
+
+    /**
+     * reference https://minecraft.fandom.com/wiki/Anvil_mechanics
+     * Experience cost = 
+            [Value of sacrificed (right placed) item] + 
+            [Work Penalty of target (left placed) item] + 
+            [Work Penalty of sacrificed (right placed) item]
+
+        New value = 
+            [Value of target (left placed) item] + 
+            [Value of sacrificed (right placed) item]. 
+     */
+    getItemEnchantPath(item: Item){
+        let path: any[] = [];
+        let steps = [];
+        let enchantments = item.enchantments;
+        let isBow = item.type.name === 'Bow';
+        let hasInfinity = enchantments.filter((item: Enchantment) => item.name === 'Infinity').length;
+
+        enchantments.sort((a: Enchantment, b: Enchantment) => a.cost - b.cost)
+
+        steps = enchantments.map((enchantment: Enchantment)=>{
+            return {
+                isItem: false,
+                name: enchantment.name,
+                value: enchantment.cost,
+                anvilUses: 0
+            };
+        });
+        
+        steps.unshift({
+            isItem: true,
+            name: item.item.name,
+            value: 0,
+            anvilUses: 0,
+            cost: 0
+        });
+
+        // mergin books and first merge of item
+        steps = calculateSteps(steps);
+
+        // lower work penalty is better
+        if( isBow && hasInfinity ){
+            while(steps.length > 1){
+                calculateSteps(steps);
+            }
+
+            return path;
+        }
+
+        // lower experience is better
+        let itemStep = steps.filter((item: any) => item.isItem).pop();
+        steps = steps.filter((item: any) => !item.isItem);
+
+        while(steps.length){
+            let next = steps.shift();
+            let workPenalty = calculateWorkPenalty(next.anvilUses) + calculateWorkPenalty(itemStep.anvilUses);
+            path.push([itemStep, next, workPenalty + next.value]);
+            itemStep = {
+                name: `${itemStep.name},${next.name}`,
+                value: itemStep.value + next.value,
+                wp: workPenalty,
+                anvilUses: Math.max.apply(null,[itemStep.anvilUses, next.anvilUses]) + 1,
+                cost: workPenalty + itemStep.value
+            };
+        }
+        
+        return path;
+
+
+        function calculateSteps(steps: Array<any>){
+            let next: Array<any> = [];
+
+            while(steps.length > 1){
+                let higher = steps.pop();
+                let lower = steps.shift();
+                let workPenalty = calculateWorkPenalty(higher.anvilUses) + calculateWorkPenalty(lower.anvilUses);
+                let value = (lower.isItem)? higher.value : lower.value;
+
+                path.push([lower, higher, workPenalty + value]);
+            
+                next.push({
+                    isItem: higher.isItem || lower.isItem,
+                    name: `${higher.name},${lower.name}`,
+                    value: higher.value + lower.value,
+                    wp: workPenalty,
+                    anvilUses: Math.max.apply(null,[higher.anvilUses, lower.anvilUses]) + 1,
+                    cost: workPenalty + value
+                });
+            }
+
+            next.sort((a: any, b: any) => a.value - b.value)
+            return next;
+        }
+
+        function calculateWorkPenalty(anvilUses: number){
+            let penalty = (Math.pow(2, anvilUses) ) - 1;
+            return (penalty > 0)? penalty : 0;
+        }
+    }
+}
