2c965f7cbdb70af7bb6620954847ca9cd6ba8929
Henrique Domingos <henrique@leoa.com.br>
2023-02-27 09:16:04 -0500

    fix: server default server setup on init and added Elytra to items

--- src/utils/Setup.ts
+++ src/utils/Setup.ts
@@ -4,20 +4,24 @@ export class Setup{
     public static init(){
         const db = new DAO();
 
-        let items = db.table('items').all().length;
-        let enchants = db.table('enchants').all().length;
-        let types = db.table('types').all().length;
+        let items = db.table('items').all().length
+        let enchants = db.table('enchants').all().length
+        let types = db.table('types').all().length
+        let servers = db.table('server').all().length
 
-        if( items && enchants && types ) return;
+
+
+        if( items && enchants && types && servers ) return;
 
         return Setup.import(db);
     }
 
     public static import(db : DAO){
-        let {enchants, types, items} = require( './default.json');
+        let {enchants, types, items} = require( './default.json')
 
-        db.table('enchants').truncate().import(enchants);
-        db.table('types').truncate().import(types);
-        db.table('items').truncate().import(items);
+        db.table('enchants').truncate().import(enchants)
+        db.table('types').truncate().import(types)
+        db.table('items').truncate().import(items)
+        db.table('server').truncate().import([{id:1, name:'default'}])
     }
 }
